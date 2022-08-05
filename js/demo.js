4fe2b308d1354e4633cc2668cc6ed072bb1563f5
Waiz-Z <80398168+Waiz-Z@users.noreply.github.com>
2022-08-05 00:19:10 -0700

    Commiting

---
+++ js/demo.js
@@ -0,0 +1,48 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '	#FF1493',
+    lineColor: '#800080'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      
+    }
+	
+  }
+  if (window.sessionStorage.getItem('reveals[i]')) {
+ document.querySelector('#reveal').classList.remove('active')
+}
+}
+
+window.addEventListener("scroll", reveal);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
